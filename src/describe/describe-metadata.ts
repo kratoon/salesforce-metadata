c3e0bcd8e65c5c42ab4d66b05807552c42978797
Ondřej Kratochvíl <ondrakratochvil96@gmail.com>
2019-12-14 17:37:06 +0100

    Add describe and find functionality.

---
+++ src/describe/describe-metadata.ts
@@ -0,0 +1,49 @@
+
+import fs from "fs-extra";
+import {join} from "path";
+import {MetadataType} from "../metadata-types";
+
+export interface MetadataDescribe {
+    directoryName: string;
+    inFolder: boolean;
+    metaFile: boolean;
+    suffix: string;
+    xmlName: string;
+    childXmlNames: any;
+}
+
+export function describeMetadata(metadata: MetadataType): MetadataDescribe | undefined {
+    const customObject: MetadataDescribe = describeCustomObject();
+    if (customObject.childXmlNames.includes(metadata)) {
+        return describeCustomObjectChild(metadata);
+    }
+    return getDescribe()?.find((it: MetadataDescribe) => it.xmlName === metadata);
+}
+
+function describeCustomObjectChild(metadata: MetadataType): MetadataDescribe | undefined {
+    if (!describedCustomObjectChildren) {
+        const path: string = join(__dirname, "..", "resources",
+            "metadata", "47", "custom-object-children-describe.json");
+        describedCustomObjectChildren = JSON.parse(fs.readFileSync(path).toString());
+    }
+    // tslint:disable-next-line:no-non-null-assertion
+    return describedCustomObjectChildren!.metadataObjects
+        .find(it => it.xmlName === metadata);
+}
+
+function describeCustomObject(): MetadataDescribe {
+    // tslint:disable-next-line:no-non-null-assertion
+    return getDescribe().find(it => it.xmlName === "CustomObject")!;
+}
+
+let described: {metadataObjects: MetadataDescribe[]} | undefined;
+let describedCustomObjectChildren: {metadataObjects: MetadataDescribe[]} | undefined;
+
+function getDescribe(): MetadataDescribe[] {
+    if (!described) {
+        const path: string = join(__dirname, "..", "resources", "metadata", "47", "describe.json");
+        described = JSON.parse(fs.readFileSync(path).toString());
+    }
+    // tslint:disable-next-line:no-non-null-assertion
+    return described!.metadataObjects;
+}
